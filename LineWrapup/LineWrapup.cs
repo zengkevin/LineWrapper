3e88e48d09a9d101cd06cce67904ae8f37b2c127
kvz <zeng.kevin@yahoo.com>
2016-09-15 22:09:02 -0700

    Green - Unit Test Passed

--- LineWrapup/LineWrapup.cs
+++ LineWrapup/LineWrapup.cs
@@ -10,7 +10,27 @@ namespace LineWrapper
     {
         public String Wrapup(string lineText, int cutColumn)
         {
-            return null;
+            string[] stringArray = lineText.Split(' ');
+
+            StringBuilder result = new StringBuilder();
+            StringBuilder newline = new StringBuilder(stringArray[0]);
+
+            for (int i=1; i<stringArray.Length; i++)
+            {
+                if(newline.Length + stringArray[i].Length < cutColumn)
+                {
+                    newline.Append(" " + stringArray[i]);
+                }
+                else
+                {
+                    result.Append(newline + "\n");
+                    newline = new StringBuilder(stringArray[i]);
+                }
+            }
+
+            result.Append(newline);
+
+            return result.ToString();
         }
     }
 }
